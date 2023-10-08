23adb23e48bbc76be83317d96498034a996b29e8
unknown <uofficial547@gmail.com>
2023-10-08 11:20:54 +0600

    first commit

---
+++ basic.js.js
@@ -0,0 +1,17 @@
+let x, y, z;
+x = 7;
+y = 8;
+z = x + y;
+document.getElementById("demo").innerHTML = z;
+
+let xx, yy, zz;
+xx = 10;
+yy = 20;
+zz = xx + yy;
+document.getElementById("demo1").innerHTML = zz;
+
+let xxx, yyy, zzz;
+xxx = 10;
+yyy = 35;
+zzz = xxx + yyy;
+document.getElementById("demo2").innerHTML = zzz;
